ccd12ada1e04ffe7df53094c16553e2034a67d73
limingy@cnfantasia.com <hyn12345>
2016-02-19 11:25:01 +0800

    add project

---
+++ ApplePayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayDemo
+//
+//  Created by mylee on 16/2/19.
+//  Copyright © 2016年 mylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayDemo
+//
+//  Created by mylee on 16/2/19.
+//  Copyright © 2016年 mylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
